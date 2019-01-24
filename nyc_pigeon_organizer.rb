6056c7b7e712929a35b7f5d09da04d907eec51c4
eselbeus <eselbeus@gmail.com>
2019-01-24 01:23:09 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  bird_hash = {}
+  data[:gender].each do |gend, birds|
+    birds.each do |bird|
+      bird_hash[bird] = {}
+    end
+  end
+  data[:color].each do |color_name, arr_birds|
+    arr_birds.each do |bird|
+      if !bird_hash[bird][:color]
+        bird_hash[bird][:color] = [color_name.to_s]
+      else
+        bird_hash[bird][:color] << color_name.to_s
+      end
+    end
+  end
+  data[:gender].each do |gender_name, arr_birds|
+    arr_birds.each do |bird|
+      bird_hash[bird][:gender] = [gender_name.to_s]
+    end
+  end
+  data[:lives].each do |location, arr_birds|
+    arr_birds.each do |bird|
+      bird_hash[bird][:lives] = [location.to_s]
+    end
+  end
+  
+  bird_hash
 end
