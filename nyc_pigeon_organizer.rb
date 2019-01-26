723d20ccb06ea136d47d5c7e39b470802f598bf2
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-26 21:44:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data[:gender].each do |sex, name|
+    name.each {|pigeon| pigeon_list[pigeon] = {color: [], gender: [], lives:[]}}
+  end
+  data.each do |category, info|
+    info.each do |trait, name|
+      name.each do |pigeon|
+        pigeon_list[pigeon][category.to_sym] << trait.to_s
+      end
+    end
+  end
+  pigeon_list
 end
